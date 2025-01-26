238668112fd555a83d5bee1b9bc3d4b4d6b945b0
poded <yxpddd@outlook.com>
2025-01-26 16:59:35 +0800

    Site updated: 2025-01-26 16:59:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/25/hello-world-1/","2025/01/25/hello-world/"];function toRandomPost(){
+var posts=["2025/01/26/欢迎信息/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
