fb6eb62b142eff22e78866c9eef496b33feedcc8
poded <yxpddd@outlook.com>
2025-01-27 00:33:11 +0800

    Site updated: 2025-01-27 00:33:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/26/欢迎信息/"];function toRandomPost(){
+var posts=["2025/01/27/Hololive死了/","2025/01/26/欢迎信息/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
