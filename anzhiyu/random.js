b9802dee31e09f777a915b674cff1bc22b80b7f8
poded <yxpddd@outlook.com>
2025-01-27 14:20:22 +0800

    Site updated: 2025-01-27 14:20:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/26/欢迎信息/","2025/01/27/Hololive死了/"];function toRandomPost(){
+var posts=["2025/01/27/Hololive死了/","2025/01/27/我好想做嘉然小姐的狗啊/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
