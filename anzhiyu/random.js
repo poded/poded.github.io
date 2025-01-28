2d0702454f8d16630186a226f2c76bd8653b8ed7
poded <yxpddd@outlook.com>
2025-01-28 12:28:12 +0800

    Site updated: 2025-01-28 12:28:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/27/Hololive死了/","2025/01/27/我好想做嘉然小姐的狗啊/"];function toRandomPost(){
+var posts=["2025/01/27/我好想做嘉然小姐的狗啊/","2025/01/27/Hololive死了/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
